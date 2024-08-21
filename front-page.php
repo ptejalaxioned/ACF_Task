8d2953945dd5d488c71a42f9615166e2c716d934
ptejalaxioned <tejalp@axioned.com>
2024-08-21 18:20:34 +0530

    index file and other file commit

--- front-page.php
+++ front-page.php
@@ -1,33 +1,30 @@
-<?php get_header()?>
- <!--main section start-->
-  <main>
-       <!--banner section start-->
-       <section class="banner">
-         <div class="wrapper">
-           <figure class="banner-image">
-             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/FFF.svg" alt="Banner" />
-           </figure>
-           <div class="banner-content">
-             <h2 class="first-caps">discover the latest trends</h2>
-             <p class="single-caps">
-               lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-               molestias pariatur vero ducimus exercitationem laudantium optio
-               minus, quis mollitia distinctio.
-             </p>
-             <div class="banner-button">
-               <a
-                 href="#FIXME"
-                 target="_self"
-                 title="Shop Now"
-                 class="first-caps"
-                 >shop now</a
-               >
-             </div>
-           </div>
-         </div>
-       </section>
-       <!--banner section ends-->
-     </main>
-     <!--main section end-->
-     <?php get_footer()?>
-
+<?php get_header() ?>
+<!--main section start-->
+<main>
+  <!--banner section start-->
+  <section class="banner">
+    <div class="wrapper">
+      <figure class="banner-image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/FFF.svg" alt="Banner" />
+      </figure>
+      <div class="banner-content">
+        <h2 class="first-caps">discover the latest trends</h2>
+        <p class="single-caps">
+          lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          molestias pariatur vero ducimus exercitationem laudantium optio
+          minus, quis mollitia distinctio.
+        </p>
+        <div class="banner-button">
+          <a
+            href="#FIXME"
+            target="_self"
+            title="Shop Now"
+            class="first-caps">shop now</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--banner section ends-->
+</main>
+<!--main section end-->
+<?php get_footer() ?>
